581309e66cc0c3bf2d1a2e8fcea99244e3d1f0ee
Pedro Zich <zichpedro22@gmail.com>
2023-08-12 12:39:48 -0300

    inital commit

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,8 @@
+export const Footer = () => {
+	return (
+		<div className="w-full h-[26px] text-white pr-32 fixed bottom-0 bg-blue items-center flex justify-end">
+			<p>Hero Tickets ® {new Date().getFullYear()} |</p>
+			<p>Todos os Direitos Reservados</p>
+		</div>
+	);
+};
